1f183cf62d67fa256cc65f33e6fa7f1039551829
volkswg <sy.siriwanjang@gmail.com>
2021-02-21 02:38:51 +0700

    implement router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./container/HomePage/HomePage";
+import LoginPage from "./container/LoginPage/LoginPage";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/login" exact component={LoginPage} />
+          <Route path="/" exact component={HomePage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/container/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class HomePage extends Component {
+  state = {};
+  render() {
+    return <div>Home</div>;
+  }
+}
+
+export default HomePage;
