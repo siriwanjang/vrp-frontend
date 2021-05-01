6996767a64657a8b93d74423642ea70e0aca186d
volkswg <sy.siriwanjang@gmail.com>
2021-05-02 03:51:20 +0700

    initial longdo map

--- src/components/general/MapComponent/MapComponent.jsx
+++ src/components/general/MapComponent/MapComponent.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
+import { longdo, map, LongdoMap } from "./LongdoMap/LongdoMap";
 
 class MapComponent extends Component {
-  state = {};
+  state = { map_obj: null };
+  initMap() {
+    map.Layers.setBase(longdo.Layers.GRAY);
+  }
+
   render() {
+    const location_list = this.props.all_location_list;
+    // console.log(location_list);
+    for (let e_loc of location_list) {
+      const lat = e_loc.location_lat;
+      const lon = e_loc.location_long;
+      const dot = new longdo.Dot(
+        {
+          lat: lat,
+          lon: lon,
+        },
+        {
+          lineWidth: 15,
+          draggable: false,
+        }
+      );
+      map.Overlays.add(dot);
+    }
+
+    const mapKey = "4b0bbb9505ee40b821fe9c0046917e4f";
     return (
       <div
         style={{
@@ -12,16 +36,7 @@ class MapComponent extends Component {
           marginBottom: 15,
         }}
       >
-        <div
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-          }}
-        >
-          MAP
-        </div>
+        <LongdoMap id="longdo-map" mapKey={mapKey} callback={this.initMap} />
       </div>
     );
   }
