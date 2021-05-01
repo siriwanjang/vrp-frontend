08636ddc0db0f521c78f0f2152f41fe90f75e1b1
volkswg <sy.siriwanjang@gmail.com>
2021-05-02 03:49:19 +0700

    add key

--- src/components/OderTrackerContent/comp_table/ComponentTable.jsx
+++ src/components/OderTrackerContent/comp_table/ComponentTable.jsx
@@ -4,15 +4,15 @@ import classes from "./ComponentTable.module.css";
 class ComponentTable extends Component {
   state = {};
   render() {
-    const th_elem = this.props.head.map((elem) => {
+    const th_elem = this.props.head.map((elem, index) => {
       let ret_elem;
       switch (elem.type) {
         case "text":
-          ret_elem = <th>{elem.text}</th>;
+          ret_elem = <th key={index}>{elem.text}</th>;
           break;
         case "checkbox":
           ret_elem = (
-            <th>
+            <th key={index}>
               <input type="checkbox" />
             </th>
           );
