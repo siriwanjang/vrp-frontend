5361e919b951e2a2364aa8d3aed58c392abffabb
volkswg <sy.siriwanjang@gmail.com>
2021-05-01 22:57:46 +0700

    redirect if not login

--- src/container/HomePage/HomePage.jsx
+++ src/container/HomePage/HomePage.jsx
@@ -2,11 +2,25 @@ import React, { Component } from "react";
 import ContainerHOC from "../../components/General/ContainerHOC/ContainerHOC";
 import Header from "../../components/General/header/Header";
 import HomePageContent from "../../components/homepage/HomePageContent";
+import Cookies from "universal-cookie";
+import { Redirect } from "react-router";
+// import api from "../../API";
 
 // import HomeContent from "../../components/homepage/content_section/HomeContent";
 class HomePage extends Component {
-  state = {};
+  state = { is_login: false };
+  componentDidMount() {
+    const cookies = new Cookies();
+    const is_logged_in = cookies.get("logged_in") === "1";
+    if (is_logged_in === false) {
+      this.setState({ is_login: true });
+    }
+  }
   render() {
+    const { is_login } = this.state;
+    if (is_login) {
+      return <Redirect to="/login" />;
+    }
     return (
       <ContainerHOC>
         <Header />
