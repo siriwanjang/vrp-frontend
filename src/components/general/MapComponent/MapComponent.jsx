92eb2eccb9581460d94ac6199326d9eb310a2ea7
volkswg <sy.siriwanjang@gmail.com>
2021-05-02 14:18:26 +0700

    handle is_show each order

--- src/components/general/MapComponent/MapComponent.jsx
+++ src/components/general/MapComponent/MapComponent.jsx
@@ -9,22 +9,42 @@ class MapComponent extends Component {
 
   render() {
     const location_list = this.props.all_location_list;
-    // console.log(location_list);
-    for (let e_loc of location_list) {
-      const lat = e_loc.location_lat;
-      const lon = e_loc.location_long;
-      const dot = new longdo.Dot(
-        {
-          lat: lat,
-          lon: lon,
-        },
-        {
-          lineWidth: 15,
-          draggable: false,
+    // if (location_list.length < 1) {
+    map.Overlays.clear();
+    // }
+    for (let e_order of location_list) {
+      console.log(e_order);
+      if (e_order.is_show === true) {
+        const lat_long_arr = [];
+        for (let e_loc of e_order.location_list) {
+          // console.log(e_loc);
+          lat_long_arr.push({ lon: e_loc.location_long, lat: e_loc.location_lat });
         }
-      );
-      map.Overlays.add(dot);
+        // console.log(lat_long_arr);
+        const polyline = new longdo.Polyline(lat_long_arr, {
+          title: "Polyline",
+          detail: "-",
+          // label: e_order.order_id,
+          lineWidth: 4,
+          lineColor: e_order.color, //"rgba(255, 0, 0, 0.8)",
+        });
+        // const lat = e_loc.location_lat;
+        // const lon = e_loc.location_long;
+        // const dot = new longdo.Dot(
+        //   {
+        //     lat: lat,
+        //     lon: lon,
+        //   },
+        //   {
+        //     lineWidth: 15,
+        //     draggable: false,
+        //   }
+        // );
+        // map.Overlays.add(dot);
+        map.Overlays.add(polyline);
+      }
     }
+    // }
 
     const mapKey = "4b0bbb9505ee40b821fe9c0046917e4f";
     return (
