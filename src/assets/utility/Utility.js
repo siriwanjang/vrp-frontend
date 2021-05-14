647bd3bf7fc381fb64f12344727101580744a02e
volkswg <sy.siriwanjang@gmail.com>
2021-05-14 21:37:45 +0700

    add parameter seperator

--- src/assets/utility/Utility.js
+++ src/assets/utility/Utility.js
@@ -1,12 +1,12 @@
 module.exports.zero_padding = (pad, num) => ("0000000" + num).substr(-pad);
 
-module.exports.datetime_converter = (datetime) => {
+module.exports.datetime_converter = (datetime, date_sep = "/") => {
   const dt_obj = new Date(datetime);
   const ret_string =
     this.zero_padding(2, dt_obj.getDate()) +
-    "/" +
+    date_sep +
     this.zero_padding(2, dt_obj.getMonth() + 1) +
-    "/" +
+    date_sep +
     dt_obj.getFullYear() +
     " " +
     this.zero_padding(2, dt_obj.getHours()) +
