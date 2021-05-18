94f1c48119dc3b45b590862da1e7c2eefca6ec87
volkswg <sy.siriwanjang@gmail.com>
2021-05-18 18:20:19 +0700

    user remote api

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: "http://app.aws.wasin.me:4000",
+  // baseURL: "http://localhost:4000",
+  baseURL: "http://app.aws.wasin.me:4000",
   headers: { "Content-Type": "application/json" },
 });
